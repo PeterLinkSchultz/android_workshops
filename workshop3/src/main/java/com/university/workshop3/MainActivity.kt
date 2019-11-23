5c0a36cb7ae7f18c7c7d0d2f461f06c7d84a2992
Daniel Sergeev <danser@yandex-team.ru>
2019-11-23 16:20:54 +0300

    add workshops4 module

--- workshop3/src/main/java/com/university/workshop3/MainActivity.kt
+++ workshop3/src/main/java/com/university/workshop3/MainActivity.kt
@@ -1,39 +1,72 @@
 package com.university.workshop3
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var rotateCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        rotateCount = savedInstanceState?.getInt(ROTATE_COUNT_KEY, -1) ?: -1
+        rotateCount += 1
+
+        bindViews()
+    }
+
+    override fun onDestroy() {
+        Log.d("MainActivity", "onDestroy called")
+        super.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(ROTATE_COUNT_KEY, rotateCount)
+    }
+
+    private fun bindViews() {
+        setInputtedValue(0)
+
+        setRotationCount(rotateCount)
+
+        bPlus.setOnClickListener {
+            val value = getInputtedValue()
+            if (value != null) {
+                setInputtedValue(value + 1)
+            }
+        }
+
+        bMinus.setOnClickListener {
+            val value = getInputtedValue()
+            if (value != null) {
+                setInputtedValue(value - 1)
+            }
+        }
+
+        bNewScreen.setOnClickListener {
+            //val intent = Intent(this, MainActivity::class.java)
+            //startActivity(intent)
+            onStop()
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    private fun setRotationCount(count: Int) {
+        tvRotateCount.text = "rotation count: $count"
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+    private fun getInputtedValue(): Int? = etValue.text.toString().toIntOrNull()
+
+    private fun setInputtedValue(value: Int) {
+        etValue.setText(value.toString())
+    }
+
+    companion object {
+        private const val ROTATE_COUNT_KEY = "ROTATE_COUNT_KEY"
     }
 }
